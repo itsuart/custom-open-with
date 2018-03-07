ba0897110f76105438f8b20613b475a4d32dee4a
Ilya Seleznev <itsuart@gmail.com>
2018-03-07 14:30:33 +0700

    ask before removing users handlers delete old code

--- src/custom-open-with.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "common.c"
-#include <windows.h>
-
-bool custom_open_with(HWND ownerWnd,const u16* title, u16 params[MAX_UNICODE_PATH_LENGTH], u16 pathToOpener[MAX_UNICODE_PATH_LENGTH], bool* pSaveConfiguration);
